fd62315b03490226e22038aec1e16300ba0c5210
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-02-14 12:28:37 -0500

    Sense Notifcation System

---
+++ Assets/Scripts/AI/Sensing/SenseNotificationSubsystem.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace AI.Sensing
+{
+    public struct SenseNotificationContext
+    {
+        public enum NotificationType
+        {
+            None,
+            Sound,
+            Sight
+        }
+        
+        public NotificationType Type;
+        public Vector3 Position;
+        public float Strength;
+
+        public SenseNotificationContext(NotificationType type, Vector3 position, float strength)
+        {
+            Type = type;
+            Position = position;
+            Strength = 0f;
+        }
+    }
+    public class SenseNotificationSubsystem
+    {
+        
+        public delegate void NotificationDelegate(SenseNotificationContext notificationContext);
+        
+        public static NotificationDelegate OnNotification;
+
+
+        public static void TriggerSoundNotification(Vector3 location, float volumeRange)
+        {
+            SenseNotificationContext context = new SenseNotificationContext
+            {
+                Type = SenseNotificationContext.NotificationType.Sound,
+                Position = location,
+                Strength = volumeRange
+            };
+            
+            OnNotification?.Invoke(context);
+        }
+    }
+}
