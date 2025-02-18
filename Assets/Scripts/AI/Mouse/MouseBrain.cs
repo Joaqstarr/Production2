70b21c9128872756c52714ef9f599b361c296fae
Joaquin Royer <joaqstarr@gmail.com>
2025-02-18 10:55:30 -0500

    Put Mouse Brain in appropriate namespace

--- Assets/Scripts/AI/Mouse/MouseBrain.cs
+++ Assets/Scripts/AI/Mouse/MouseBrain.cs
@@ -1,26 +1,16 @@
-using AI.Sensing;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
-namespace AI.Mouse
+public class MouseBrain : MonoBehaviour
 {
-    public class MouseBrain : MonoBehaviour
+    void Start()
     {
-        private GenericDistanceListener _laserListener;
+        
+    }
 
-        private NavMeshAgent _agent;
-        // Start is called before the first frame update
-        void Start()
-        {
-            _laserListener = new GenericDistanceListener(SenseNotificationContext.NotificationType.Laser, 5, transform);
-            _laserListener.OnNotificationReceived += OnLaserReceived;
-            _agent = GetComponent<NavMeshAgent>();
-        }
-
-
-        private void OnLaserReceived(SenseNotificationContext notification)
-        {
-            _agent.SetDestination(notification.Position);
-        }
+    void Update()
+    {
+        
     }
 }
