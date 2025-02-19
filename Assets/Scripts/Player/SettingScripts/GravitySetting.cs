57c84abf59d246ecd4f84a9ab661a1d4ba5ae78d
Matterjaws55 <163688327+Matterjaws55@users.noreply.github.com>
2025-02-19 16:44:15 -0500

    Player Movement and Setting Changes

--- Assets/Scripts/Player/SettingScripts/GravitySetting.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "GravitySettings", menuName = "Player/GravitySettings")]
-public class GravitySettings : ScriptableObject
-{
-    public float gravity = -9.81f;
-}
--- Assets/Scripts/Player/SettingScripts/MovementSetting.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "MovementSettings", menuName = "Player/MovementSettings")]
-public class MovementSettings : ScriptableObject
-{
-    public float walkSpeed = 5f;
-}
