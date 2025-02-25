c7a7f8536d2a8c6130272167163adf1676bb8972
Matterjaws55 <163688327+Matterjaws55@users.noreply.github.com>
2025-02-25 18:49:30 -0500

    Jump fix and footsteps

---
+++ Assets/Scripts/Player/PlayerAudio.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    public AudioClip[] footstepSounds;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayFootstep()
+    {
+        if (footstepSounds.Length > 0)
+        {
+            AudioClip clip = footstepSounds[Random.Range(0, footstepSounds.Length)];
+            audioSource.PlayOneShot(clip);
+        }
+    }
+}
