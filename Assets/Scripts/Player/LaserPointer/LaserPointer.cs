b0e1a00ce2a1c2681c200c235258513b6442515c
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-02-25 01:32:56 -0500

    Laser Beam art

--- Assets/Scripts/Player/LaserPointer/LaserPointer.cs
+++ Assets/Scripts/Player/LaserPointer/LaserPointer.cs
@@ -1,42 +1,66 @@
 using System;
 using AI.Sensing;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 namespace Player.LaserPointer
 {
     public class LaserPointer : MonoBehaviour
     {
+        private static readonly int ClickLaserProperty = Animator.StringToHash("ClickLaser");
+        private static readonly int IsHoldingLaserPointer = Animator.StringToHash("IsHoldingLaserPointer");
         private PlayerControls _controls;
 
         [SerializeField]
         private Transform _lookTransform;
+        [SerializeField]
+        private TwoBoneIKConstraint _laserHoldConstraint;
+
+        [SerializeField] private LaserBeam _laserPointerObject;
+        private Animator _animator;
 
         [SerializeField] private LayerMask _laserHitMask;
-        private void OnEnable()
+
+        private void Awake()
         {
             _controls = GetComponent<PlayerControls>();
+            _animator = GetComponent<Animator>();
+        }
 
+        private void OnEnable()
+        {
+            _laserPointerObject.gameObject.SetActive(true);
+            
+            _laserHoldConstraint.weight = 1;
+
+            _animator.SetBool(IsHoldingLaserPointer, true);
             _controls.OnLaserPressed += OnLaserPressed;
             _controls.OnLaserUnpressed += OnLaserUnpressed;
         }
 
         private void OnDisable()
         {
+            _laserPointerObject.gameObject.SetActive(false);
+
+            _animator.SetBool(IsHoldingLaserPointer, false);
+            _laserHoldConstraint.weight = 0;
             _controls.OnLaserPressed -= OnLaserPressed;
             _controls.OnLaserUnpressed -= OnLaserUnpressed;
         }
 
         // Start is called before the first frame update
-        void Start()
-        {
-        }
-
         private void OnLaserUnpressed()
         {
+            _animator.SetTrigger(ClickLaserProperty);
+            _laserPointerObject.DisableBeam();
+
         }
 
         private void OnLaserPressed()
         {
+            _laserPointerObject.EnableBeam();
+            
+            _animator.SetTrigger(ClickLaserProperty);
         }
 
         // Update is called once per frame
