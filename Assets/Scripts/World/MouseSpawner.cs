c0504b1022e4fbb852bcc6a61864190ffeeadf72
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-03-03 09:41:02 -0500

    Placed spawners and mouse spawner line of sight check

--- Assets/Scripts/World/MouseSpawner.cs
+++ Assets/Scripts/World/MouseSpawner.cs
@@ -1,9 +1,7 @@
-using System;
 using AI.Sensing;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.Serialization;
 
 namespace World
 {
@@ -15,8 +13,8 @@ namespace World
         [SerializeField] private float _mouseJumpHeight = 2;
         [SerializeField] private float _mouseJumpTime = 1;
         [SerializeField] private float _laserSenseRange = 3;
-
-
+        [SerializeField] private Transform _rayCastPoint;
+        [SerializeField] private LayerMask _environmentLayerMask;
         private NavMeshAgent _mouse;
         
         private void Start()
@@ -27,12 +25,12 @@ namespace World
 
         private void OnNotificationReceived(SenseNotificationContext notification)
         {
-            if(_mouse == null)
+            if (_mouse ==null && LineOfSightCheck(notification.Position))
             {
                 NavMeshQueryFilter filter = new NavMeshQueryFilter();
                 filter.agentTypeID = _mousePrefab.agentTypeID; // Ensure the agent type ID matches the prefab
                 filter.areaMask = NavMesh.AllAreas; // Ensure the area mask matches the prefab
-                
+
                 Vector3 dir = (notification.Position - transform.position).normalized;
                 Vector3 mouseJumpRawPos = transform.position + (dir * _mouseJumpDist);
                 NavMeshHit hit;
@@ -45,7 +43,7 @@ namespace World
                     _mouse = Instantiate(_mousePrefab, transform.position, Quaternion.identity);
                     _mouse.enabled = false;
                     _mouse.transform.forward = dir;
-                    
+
                     _mouse.transform.DOJump(hit.position, _mouseJumpHeight, 1, _mouseJumpTime).OnComplete(() =>
                     {
                         _mouse.enabled = true;
@@ -53,11 +51,30 @@ namespace World
                 }
                 else
                 {
-                    Debug.LogWarning("NavMesh.SamplePosition failed to find a valid position.");
+                   // Debug.LogWarning("NavMesh.SamplePosition failed to find a valid position.");
                 }
 
             }
+        }
+
+        private bool LineOfSightCheck(Vector3 pos)
+        {
+            RaycastHit hit;
+            Vector3 direction = (pos - _rayCastPoint.position).normalized;
+            if (Physics.Raycast(_rayCastPoint.position, direction, out hit, 6, _environmentLayerMask))
+            {
+                float dist = Vector3.Distance(hit.point, pos);
+                // Check if the hit object is the target position
+                if (dist < 0.5f)
+                {
+                    return true;
+                }
+
+                return false;
+            }
             
+            
+            return true;
         }
     }
 }
